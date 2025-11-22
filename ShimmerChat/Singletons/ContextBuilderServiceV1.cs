ebf1853e9ec436b96a8829536395694f8cb1d190
Kas1o <twins_t@outlook.com>
2025-11-22 15:30:55 +0800

    refactor(ContextBuilderServiceV1): 将Utils中的方法移动到ContextBuilderServiceV1内部
    将ToPromptBuilder方法从Utils类移动到ContextBuilderServiceV1类中，并重命名为CreatePromptBuilder，以提高代码的内聚性和可维护性

--- ShimmerChat/Singletons/ContextBuilderServiceV1.cs
+++ ShimmerChat/Singletons/ContextBuilderServiceV1.cs
@@ -2,11 +2,44 @@ using SharperLLM.Util;
 using SharperLLM.FunctionCalling;
 using ShimmerChatLib;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShimmerChat.Singletons
 {
     public class ContextBuilderServiceV1 : IContextBuilderService
     {
+        /// <summary>
+        /// 将聊天转换为PromptBuilder
+        /// </summary>
+        /// <param name="chat">聊天对象</param>
+        /// <param name="system">系统提示</param>
+        /// <param name="tools">可选的工具列表</param>
+        /// <returns>构建好的PromptBuilder</returns>
+        private PromptBuilder CreatePromptBuilder(Chat chat, string system, List<Tool> tools = null)
+        {
+            var pb = new PromptBuilder();
+            var p = chat.Messages.Select(
+                x =>
+                     x.sender.ToLower() switch
+                    {
+                        "user" => (x.message, PromptBuilder.From.user),
+                        "system" => (x.message, PromptBuilder.From.system),
+                        "ai" => (x.message, PromptBuilder.From.assistant),
+                        "tool_call" => (x.message, PromptBuilder.From.tool_call),
+                        "tool_result" => (x.message, PromptBuilder.From.tool_result),
+                        var n => throw new InvalidOperationException($"Unsupported sender Type: {n}")
+                    }
+            ).ToList();
+            p.Insert(0, (system, PromptBuilder.From.system));
+            pb.Messages = p.ToArray();
+            if(tools != null)
+            {
+                pb.AvailableTools = tools;
+                pb.AvailableToolsFormatter = ToolPromptParser.Parse;
+            }
+            return pb;
+        }
+
         /// <summary>
         /// 为聊天构建PromptBuilder
         /// </summary>
@@ -15,7 +48,7 @@ namespace ShimmerChat.Singletons
         /// <returns>构建好的PromptBuilder</returns>
         public PromptBuilder BuildPromptBuilder(Chat chat, string agentDescription)
         {
-            return chat.ToPromptBuilder(agentDescription);
+            return CreatePromptBuilder(chat, agentDescription);
         }
         
         /// <summary>
@@ -27,7 +60,7 @@ namespace ShimmerChat.Singletons
         /// <returns>构建好的PromptBuilder</returns>
         public PromptBuilder BuildPromptBuilderWithTools(Chat chat, string agentDescription, List<Tool> toolDefinitions)
         {
-            return chat.ToPromptBuilder(agentDescription, toolDefinitions);
+            return CreatePromptBuilder(chat, agentDescription, toolDefinitions);
         }
     }
 }
