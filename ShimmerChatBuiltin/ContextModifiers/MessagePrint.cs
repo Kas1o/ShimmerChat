1114a94e540e10a7a7ca32e51bb2b41a08b6e759
Kas1o <twins_t@outlook.com>
2025-12-04 14:05:15 +0800

    feat(ShimmerChatBuiltin): 添加MessagePrint上下文修改器
    实现一个新的上下文修改器，用于将消息内容以JSON格式打印到控制台

---
+++ ShimmerChatBuiltin/ContextModifiers/MessagePrint.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SharperLLM.Util;
+using ShimmerChatLib.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShimmerChatBuiltin.ContextModifiers
+{
+	public class MessagePrint : IContextModifier
+	{
+		private JsonSerializerSettings Settings;
+		public MessagePrint()
+		{
+			Settings = new JsonSerializerSettings();
+			Settings.Converters.Add(new StringEnumConverter());
+		}
+
+		ContextModifierInfo IContextModifier.info => new ContextModifierInfo
+		{
+			Name = "MessagePrint",
+			Description = "print message dump to console. input will be ignored.",
+		};
+
+		void IContextModifier.ModifyContext(PromptBuilder promptBuilder, string input)
+		{
+			var json = JsonConvert.SerializeObject(promptBuilder.Messages, Settings);
+			Console.WriteLine(json);
+		}
+	}
+}
