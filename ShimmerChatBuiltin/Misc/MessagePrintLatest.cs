8d2015a90576c3755605106e8914b67d41de36ba
Kas1o <twins_t@outlook.com>
2026-06-09 17:03:01 +0800

    添加一个Message Print Latest 工具（原型）

---
+++ ShimmerChatBuiltin/Misc/MessagePrintLatest.cs
@@ -0,0 +1,34 @@
+﻿using ShimmerChatLib;
+using ShimmerChatLib.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShimmerChatBuiltin.Misc
+{
+	public class MessagePrintLatestConfig : ModifierConfig
+	{
+
+	}
+
+	public class MessagePrintLatest : IContextModifier
+	{
+		public ContextModifierInfo info => new ContextModifierInfo
+		{
+			Name = "MessagePrintLatest",
+			Description = "Print the latest message content"
+		};
+
+		public Type ConfigType => typeof(MessagePrintLatestConfig);
+
+		public void ModifyContext(ContextDocument context, ModifierConfig config, Chat chat, Agent agent)
+		{
+			Console.WriteLine(context.Segments.Last()?.Message.Content);
+		}
+
+		public (bool IsValid, string Error) Validate(ModifierConfig config)
+		{
+			return (true, null);
+		}
+	}
+}
